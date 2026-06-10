4b865c80d118cc550ee1c537881f4f5272b20acb
ayoolaowoilu <ayoolaowoilu@gmail.com>
2026-06-10 17:51:23 +0100

    Chore : Started writing the apis for my the admin stats

--- app/api/ad45667899/7822/route.ts
+++ app/api/ad45667899/7822/route.ts
@@ -1,9 +1,11 @@
+import db from "@/app/lib/DBschema";
 
 
 export async function GET(){
      
     try{
-     const [row] = await db.query
+     const [row] = await db.query("SELECT COUNT(*) as count FROM users ")
+     const [row1] = await db.query("SELECT COUNT(*) as count FROM centers")
     }catch(error){
        
     }
